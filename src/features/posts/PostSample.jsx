1419e3aec39847d9e36642aed827feda9c890bce
Dan McGrath <mcgrathd3254@yahoo.com>
2024-05-08 17:36:06 -0400

    create ternary to display correct media if available

--- src/features/posts/PostSample.jsx
+++ src/features/posts/PostSample.jsx
@@ -3,10 +3,38 @@ import { faDownLong, faUpLong } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const PostSample = ({ post }) => {
+  if (post.secure_media) {
+    console.log(post.secure_media.reddit_video.dash_url);
+  }
+
   return (
     <>
       <h3>{post.title}</h3>
       <h4>{post.subreddit_name_prefixed}</h4>
+      {post.secure_media ? (
+        <>
+          <video
+            autoPlay
+            muted
+            controls
+            className="max-w-full m-auto max-h-dvh"
+          >
+            <source
+              src={post.secure_media.reddit_video.fallback_url}
+              type="video/mp4"
+            />
+          </video>
+        </>
+      ) : (
+        post.preview && (
+          <>
+            <img
+              src={post.preview.images[0].source.url}
+              className="max-w-full m-auto"
+            />
+          </>
+        )
+      )}
       <div className="flex items-center">
         <div className="flex justify-around max-w-28 items-center bg-gray rounded-full my-2">
           <FontAwesomeIcon
