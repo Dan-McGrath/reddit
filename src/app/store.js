698201e05b1599fd1dbdc5c1a034f0f814f19074
Dan McGrath <mcgrathd3254@yahoo.com>
2024-05-07 20:14:57 -0400

    add postsSlice reducer to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
+import postReducer from "../features/posts/postSlice";
 
 export default configureStore({
-  reducer: {},
+  reducer: { posts: postReducer },
 });
