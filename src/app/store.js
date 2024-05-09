87e42097fd8ba443488149216bc1e262731ef71e
Dan McGrath <mcgrathd3254@yahoo.com>
2024-05-08 21:48:49 -0400

    add search to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import postReducer from "../features/posts/postSlice";
+import searchReducer from "../features/search/searchSlice";
 
 export default configureStore({
-  reducer: { posts: postReducer },
+  reducer: { posts: postReducer, search: searchReducer },
 });
