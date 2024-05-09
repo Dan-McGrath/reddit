4ea041f2fb8f8e80573d87fbb06986a4cc76bc8c
Dan McGrath <mcgrathd3254@yahoo.com>
2024-05-09 19:58:23 -0400

    create post detail. add redux functions

--- src/features/posts/PostDetail.jsx
+++ src/features/posts/PostDetail.jsx
@@ -1,5 +1,43 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  loadAllComments,
+  selectAllComments,
+  isLoading,
+} from "../comments/commentSlice";
+import Post from "./Post";
+import Comment from "../comments/Comment";
+
 const PostDetail = () => {
-  return <div>PostDetail</div>;
+  const dispatch = useDispatch();
+  const { id } = useParams();
+
+  useEffect(() => {
+    dispatch(loadAllComments(id));
+  }, [dispatch, id]);
+
+  const detail = useSelector(selectAllComments);
+  const isLoadingComments = useSelector(isLoading);
+
+  if (isLoadingComments) {
+    return <div>Loading...</div>;
+  }
+
+  const { postInfo, comments } = detail;
+
+  return (
+    <>
+      <Post post={postInfo} />
+      <div>
+        {comments.map((comment) => (
+          <>
+            <Comment comment={comment} />
+          </>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default PostDetail;
