bfb0cd039ec5fd066ea64b825ee6b4963e5cd4c7
Dan McGrath <mcgrathd3254@yahoo.com>
2024-05-08 20:07:50 -0400

    add Searchbar to header

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,12 +1,17 @@
 import { faReddit } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import SearchBar from "../../features/search/SearchBar";
+
 const Header = () => {
   return (
-    <header className="bg-gray">
+    <header className="bg-gray grid grid-cols-12 gap-3 items-center justify-items-center">
       <FontAwesomeIcon
         icon={faReddit}
-        className="text-3xl text-logo bg-white rounded-full m-3"
+        className="text-4xl text-logo bg-white rounded-full m-3 col-end-2"
       />
+      <div className="col-start-3 col-end-11 flex min-w-full m-auto justify-center">
+        <SearchBar />
+      </div>
     </header>
   );
 };
