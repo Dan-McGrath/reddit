31213e8423b6ff5702dc20e01a328831257452ea
Dan McGrath <mcgrathd3254@yahoo.com>
2024-05-09 19:57:07 -0400

    add commentReducer to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import postReducer from "../features/posts/postSlice";
 import searchReducer from "../features/search/searchSlice";
+import commentReducer from "../features/comments/commentSlice";
 
 export default configureStore({
-  reducer: { posts: postReducer, search: searchReducer },
+  reducer: {
+    posts: postReducer,
+    search: searchReducer,
+    comments: commentReducer,
+  },
 });
