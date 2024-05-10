715b0a61aae60ae1d6b0d201de3328da7f2c4b89
Dan McGrath <mcgrathd3254@yahoo.com>
2024-05-09 20:02:26 -0400

    add link to route to homepage

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,15 +1,18 @@
 import { faReddit } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import SearchBar from "../../features/search/SearchBar";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className="bg-gray grid grid-cols-12 gap-3 items-center justify-items-center">
-      <FontAwesomeIcon
-        icon={faReddit}
-        className="text-4xl text-logo bg-white rounded-full m-3 col-end-2"
-      />
-      <div className="col-start-3 col-end-11 flex min-w-full m-auto justify-center">
+    <header className="grid items-center grid-cols-12 gap-3 bg-gray justify-items-center">
+      <Link to="/">
+        <FontAwesomeIcon
+          icon={faReddit}
+          className="col-end-2 m-3 text-4xl bg-white rounded-full text-logo"
+        />
+      </Link>
+      <div className="flex justify-center min-w-full col-start-3 col-end-11 m-auto">
         <SearchBar />
       </div>
     </header>
