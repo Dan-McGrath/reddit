44662f090f635b961d71510846b29969d678c851
Dan McGrath <mcgrathd3254@yahoo.com>
2024-05-10 19:40:03 -0400

    Add searchResults component map over array

--- src/features/search/SearchBar.jsx
+++ src/features/search/SearchBar.jsx
@@ -1,40 +1,58 @@
-import { faMagnifyingGlass, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useDispatch, useSelector } from "react-redux";
 import {
-  clearSearchTerm,
   selectSearchTerm,
   setSearchTerm,
+  loadSearchResults,
+  selectSearchResults,
 } from "./searchSlice";
+import { useEffect } from "react";
+import SearchResults from "./SearchResults";
 
 const SearchBar = () => {
   const dispatch = useDispatch();
   const searchTerm = useSelector(selectSearchTerm);
+  const searchResults = useSelector(selectSearchResults);
 
   const onSearchChangeHandler = (e) => {
     dispatch(setSearchTerm(e.target.value));
   };
 
+  useEffect(() => {
+    dispatch(loadSearchResults(searchTerm));
+  }, [searchTerm, dispatch]);
+
   return (
     <>
-      <search className="min-w-fit">
+      <search className="my-2 min-w-fit">
         <label className="relative block">
           <span className="sr-only">Search</span>
           <span className="absolute inset-y-0 left-0 flex items-center pl-2">
             <FontAwesomeIcon
               icon={faMagnifyingGlass}
-              className="h-5 w-5 text-dark-gray"
+              className="w-5 h-5 text-dark-gray"
             />
           </span>
           <input
             type="search"
             placeholder="Search Reddit"
-            className="placeholder:italic placeholder:text-dark-gray block bg-white w-full border border-dark-gray rounded-full py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-logo focus:ring-logo focus:ring-1 sm:text-sm text-dark-gray min-w-96"
+            className="block w-full py-2 pr-3 bg-white border rounded-full shadow-sm placeholder:italic placeholder:text-dark-gray border-dark-gray pl-9 focus:outline-none focus:border-logo focus:ring-logo focus:ring-1 sm:text-sm text-dark-gray min-w-96"
             onChange={onSearchChangeHandler}
             value={searchTerm}
           />
         </label>
       </search>
+      {searchResults.length > 0 && (
+        <div className="absolute z-10 block p-2 mx-auto overflow-scroll overflow-x-hidden rounded-lg top-20 max-w-96 bg-gray max-h-96">
+          {searchResults.map((result) => (
+            <div key={result.data.id} className="py-1 ">
+              <SearchResults result={result} />
+              <hr className="my-1 text-light-gray/40" />
+            </div>
+          ))}
+        </div>
+      )}
     </>
   );
 };
