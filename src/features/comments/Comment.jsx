d39e5706dbdbe2368373786547425436285b4372
Dan McGrath <mcgrathd3254@yahoo.com>
2024-05-09 21:52:10 -0400

    add data to comment component

--- src/features/comments/Comment.jsx
+++ src/features/comments/Comment.jsx
@@ -1,5 +1,35 @@
+import { faMessage } from "@fortawesome/free-regular-svg-icons";
+import {
+  faDownLong,
+  faUpLong,
+  faCircle,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { formatDistanceStrict, fromUnixTime } from "date-fns";
+
 const Comment = ({ comment }) => {
-  return <div>Comment</div>;
+  const today = Date.now();
+
+  let created;
+  if (comment.data.created) {
+    created = fromUnixTime(comment.data.created);
+  } else if (comment.data.created_utc) {
+    created = fromUnixTime(comment.data.created_utc);
+  } else {
+    created = Date.now();
+  }
+  return (
+    <div className="">
+      <div>
+        <p>{comment.data.author}</p>
+        <div className="flex items-center gap-2 text-light-gray">
+          <FontAwesomeIcon icon={faCircle} className="w-1 text-white" />
+          <p>{formatDistanceStrict(today, created)} ago</p>
+        </div>
+      </div>
+      <p>{comment.data.body}</p>
+    </div>
+  );
 };
 
 export default Comment;
