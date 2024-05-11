c56305e59a5c68378d2357185b5422bc7a01c50c
Dan McGrath <mcgrathd3254@yahoo.com>
2024-05-10 22:02:28 -0400

    add responsive styling to header

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -5,14 +5,14 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className="grid items-center grid-cols-12 gap-3 py-1 bg-gray justify-items-center">
+    <header className="grid items-center grid-cols-12 py-1 md:gap-3 bg-gray justify-items-center">
       <Link to="/">
         <FontAwesomeIcon
           icon={faReddit}
-          className="col-end-2 m-3 text-5xl bg-white border-2 rounded-full border-logo text-logo"
+          className="col-end-2 m-3 text-3xl bg-white border-2 rounded-full md:text-4xl lg:text-5xl border-logo text-logo"
         />
       </Link>
-      <div className="flex justify-center min-w-full col-start-3 col-end-11 m-auto">
+      <div className="flex justify-center col-start-3 col-end-11 m-auto md:max-w-40 max-w-96 lg:max-w-fit">
         <SearchBar />
       </div>
     </header>
